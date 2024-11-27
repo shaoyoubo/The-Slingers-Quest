c86dc5ed2d2e6f032b590a131149fc8a5032fdfa
shaoyoubo <shaoyoubo@outlook.com>
2024-11-27 13:27:16 +0800

    adjust loadWorld

--- game/loadWorld.js
+++ game/loadWorld.js
@@ -7,7 +7,7 @@ import { OctreeHelper } from 'three/addons/helpers/OctreeHelper.js';
 
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 const loader = new GLTFLoader().setPath('../Assets/');
-
+const repeatScale = 0.1;
 export function loadWorldModel(scene, worldOctree) {
     const textureLoader = new THREE.TextureLoader();
 
@@ -15,9 +15,12 @@ export function loadWorldModel(scene, worldOctree) {
     const colorMap = textureLoader.load('../Assets/textures/Rock051_1K-PNG_Color.png');
     const normalMap = textureLoader.load('../Assets/textures/Rock051_1K-PNG_NormalGL.png');
     const roughnessMap = textureLoader.load('../Assets/textures/Rock051_1K-PNG_Roughness.png');
-    colorMap.repeat.set(0.001, 0.001);
-    normalMap.repeat.set(0.001, 0.001);
-    roughnessMap.repeat.set(0.001, 0.001);
+    colorMap.wrapS = colorMap.wrapT = THREE.RepeatWrapping;
+    normalMap.wrapS = normalMap.wrapT = THREE.RepeatWrapping;
+    roughnessMap.wrapS = roughnessMap.wrapT = THREE.RepeatWrapping;
+    colorMap.repeat.set(repeatScale, repeatScale);
+    normalMap.repeat.set(repeatScale, repeatScale);
+    roughnessMap.repeat.set(repeatScale, repeatScale);
     loader.load('./Environment/collision-world.glb', (gltf) => {
         // 将加载的模型添加到场景中
         scene.add(gltf.scene);
@@ -34,8 +37,8 @@ export function loadWorldModel(scene, worldOctree) {
                 // 创建基础材质并应用纹理
                 const material = new THREE.MeshStandardMaterial({
                     map: colorMap, // 颜色贴图
-                    //normalMap: normalMap, // 法线贴图
-                    //roughnessMap: roughnessMap, // 粗糙度贴图
+                    normalMap: normalMap, // 法线贴图
+                    roughnessMap: roughnessMap, // 粗糙度贴图
                 });
 
                 // 为网格应用材质
